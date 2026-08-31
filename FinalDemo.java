6e5e321ae329af05d7f2085785a3b54ef50e6ec5
Shubham Jha <ishubham0666@gmail.com>
2026-08-31 22:06:58 +0530

    Static Demo

---
+++ FinalDemo.java
@@ -0,0 +1,22 @@
+class Student
+{
+    int rollno;
+    Student(int r)
+    {
+        rollno = r;
+    }
+
+    void studisp()
+    {
+        System.out.println("Roll No is : " + rollno);
+    }
+}
+
+public class FinalDemo
+{
+  public static void main(String[] args) {
+      Student s1 = new Student(52);
+
+      s1.studisp();
+  }
+}
